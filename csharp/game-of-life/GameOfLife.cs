8727dc7f9386685fc1c441d2fa2ec55fa738756f
Andor Sípos <53154407+sipos-andor@users.noreply.github.com>
2024-09-11 21:27:42 +0200

    Rename `newMatrix` to `nextGeneration` in `Tick` method
    Renamed the variable `newMatrix` to `nextGeneration` throughout the `Tick` method in the `GameOfLife` class. This change includes the variable's declaration, its assignment within the nested loops, and the return statement. The new name better reflects the variable's purpose, representing the state of the game board in the next generation, thereby improving code readability and maintainability.

--- csharp/game-of-life/GameOfLife.cs
+++ csharp/game-of-life/GameOfLife.cs
@@ -4,32 +4,32 @@ public static class GameOfLife
     {
         int xLength = matrix.GetLength(0);
         int yLength = matrix.GetLength(1);
-        int[,] newMatrix = new int[xLength, yLength];
+        int[,] nextGeneration = new int[xLength, yLength];
         for (int i = 0; i < xLength; ++i)
         {
             for (int j = 0; j < yLength; ++j)
             {
-                int neighbors = 0;
+                int liveNeighbors = 0;
                 int x = i - 1;
                 for (x = x < 0 ? 0 : x; x <= i + 1 && x < xLength; ++x)
                 {
                     int y = j - 1;
                     for (y = y < 0 ? 0 : y; y <= j + 1 && y < yLength; ++y)
                     {
-                        if (matrix[x, y] == 1 && !(x == i && y == j) && ++neighbors > 3)
+                        if (matrix[x, y] == 1 && !(x == i && y == j) && ++liveNeighbors > 3)
                         {
                             break;
                         }
                     }
                 }
 
-                if (neighbors == 2 && matrix[i, j] == 1 || neighbors == 3)
+                if (liveNeighbors == 2 && matrix[i, j] == 1 || liveNeighbors == 3)
                 {
-                    newMatrix[i, j] = 1;
+                    nextGeneration[i, j] = 1;
                 }
             }
         }
 
-        return newMatrix;
+        return nextGeneration;
     }
 }
